c46f4b693157d08efd6ab907858548a85940d6d0
Eric-dev13 <lanzae32@gmail.com>
2023-06-06 15:14:42 +0200

    Stocker les tâches dans un tableau.

--- assets/js/todolist.js
+++ assets/js/todolist.js
@@ -2,20 +2,30 @@ let taches = [{
     id: 1,
     titre: "faire l'appli",
     description: "travail d'équipe",
-    date: "",
+    date: new Date(),
     etat: 0
 },
 {
     id: 2,
     titre: "modifier l'appli",
     description: "ça continue",
-    date: "",
+    date: new Date(),
     etat: 0
 }
 ];
 
+let tableTodo = document.querySelector('.tableTodo');
 let todolist = document.querySelector('#todolist');
+
 taches.forEach((tache) => {
-    todolist.innerHTML += `<li class="list-group-item">${tache.titre} <br> ${tache.description} </li>`;
-    
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    todolist.innerHTML += `
+    <li class="list-group-item border-dark" data-id="${tache.id}">
+        <span class="fw-semibold">Titre</span>: ${tache.titre}<br>
+        <span class="fw-semibold">Description</span>: ${tache.description}<br>
+        <span class="fw-semibold">Date</span>: ${tache.date.toLocaleDateString('fr-FR', options)}
+    </li>
+    `;
 });
+
+
