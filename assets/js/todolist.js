a9b288acc46c60ed0fe1d19214160d8bdd857b00
catherinejj <catherine-jules@outlook.com>
2023-06-06 13:43:12 +0200

    premiere version sur ma branch

--- assets/js/todolist.js
+++ assets/js/todolist.js
@@ -16,6 +16,21 @@ let taches = [{
 
 let todolist = document.querySelector('#todolist');
 taches.forEach((tache) => {
-    todolist.innerHTML += `<li class="list-group-item">${tache.titre} <br> ${tache.description} </li>`;
+    todolist.innerHTML += `<li id="${tache.id}" class="list-group-item">${tache.titre} <br> ${tache.description} </li>`;
     
 });
+
+/*taches.forEach((tache) => {
+    //i = l'id de l'element
+    var i=tache.id;
+    //pour l'element dont i est l'id
+    const identifiant = document.getElementById(i);
+    //a chaque click sur l'element
+    identifiant.addEventListener("click", function(){
+        //on ajoute une classe
+        identifiant.classList.add("fait");
+        alert(i);
+    }
+    );
+});
+/**/
