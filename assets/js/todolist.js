a0c70441ed092b9d8dd33762b848a7224295bacf
hugoMontevideo <machadopereira.hugo@gmail.com>
2023-06-06 15:19:05 +0200

    details

--- assets/js/todolist.js
+++ assets/js/todolist.js
@@ -1,21 +1,49 @@
 let taches = [{
-    id: 1,
-    titre: "faire l'appli",
-    description: "travail d'équipe",
-    date: "",
-    etat: 0
-},
-{
-    id: 2,
-    titre: "modifier l'appli",
-    description: "ça continue",
-    date: "",
-    etat: 0
-}
+        id: 1,
+        titre: "faire l'appli",
+        description: "travail d'équipe",
+        date: "",
+        etat: 0
+    },
+    {
+        id: 2,
+        titre: "modifier l'appli",
+        description: "ça continue",
+        date: "",
+        etat: 0
+    }
 ];
 
 let todolist = document.querySelector('#todolist');
+
+// ********************
+// *******   FUNCTIONS      *************
+// ********************
+// ********************
+
+
+
+
+
+
+
+
+// *************************************
+// *******   CODE      *************
+// *************************************
+// *************************************
+
+// Afficher les taches ( a partir du tableau taches)
 taches.forEach((tache) => {
     todolist.innerHTML += `<li class="list-group-item">${tache.titre} <br> ${tache.description} </li>`;
     
 });
+
+displayTasks()
+
+
+
+
+
+
+
