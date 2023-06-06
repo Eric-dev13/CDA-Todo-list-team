12db57ddcb899e5c2582aa4094a310aa1c2487f4
Eric-dev13 <lanzae32@gmail.com>
2023-06-06 11:24:58 +0200

    Debut du sprint

---
+++ assets/js/todolist.js
@@ -0,0 +1,17 @@
+let taches = [{
+    id: 1,
+    titre: "faire l'appli",
+    description: "travail d'équipe",
+    date: "",
+    etat: 0
+},
+{
+    id: 2,
+    titre: "modifier l'appli",
+    description: "ça continue",
+    date: "",
+    etat: 0
+}
+
+
+];
