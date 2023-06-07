7e0f84bdebfe11ce203d50cb0760f3f54d1046b9
LovaHenin <ratovolova@yahoo.com>
2023-06-07 14:59:14 +0200

    tableau dynamique et fonction tachefait

--- assets/js/todolist.js
+++ assets/js/todolist.js
@@ -3,23 +3,11 @@
 // **************************************
 
 const taches = [
-    {
-        id: 1,
-        titre: "faire l'appli",
-        description: "travail d'équipe",
-        date: new Date(),
-        etat: 0
-    },
-    {
-        id: 2,
-        titre: "modifier l'appli",
-        description: "ça continue",
-        date: new Date(),
-        etat: 0
-    }
+  
 ];
 
 const todolist = document.querySelector('#todolist');
+let compteur = 0;
 
 // ****************************************
 // **********     FUNCTIONS  **************
@@ -32,11 +20,11 @@ function insertTask() {
 
     // Création d'un objet avec les valeurs récupérées
     let new_task = {}
-    new_task.id=3;
-    new_task.titre=titre;
-    new_task.description=description;
-    new_task.date=new Date();
-    new_task.etat=0;
+    new_task.id = 3;
+    new_task.titre = titre;
+    new_task.description = description;
+    new_task.date = new Date();
+    new_task.etat = 0;
 
     taches.push(new_task); // Ajout de l'objet dans le tableau taches
     displayTasks(taches);
@@ -44,9 +32,9 @@ function insertTask() {
 
 /**  Afficher / raffraichir les taches
  *  @param array taches[]
- **/ 
-function displayTasks(taches){
-    todolist.innerHTML='';
+ **/
+function displayTasks(taches) {
+    todolist.innerHTML = '';
     var index = 1;
     taches.forEach((tache) => {
         const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
@@ -60,31 +48,52 @@ function displayTasks(taches){
             </li>
         `;
         index++;
-    });   
+    });
+    tacheFait();
+    
 }
 
 
+function compteurTacheFait( taches ) {
+    //tacheFait()
+    taches.forEach((tache1) => {
+        if (tache1.etat == 1) {
+            compteur += 1;
+        }
+    });
+    return compteur;
+}
+
+
+function tacheFait() {
+    taches.forEach((tache) => {
+        //i = l'id de l'element
+        var i = tache.id;
+        //pour l'element dont i est l'id
+        const identifiant = document.getElementById(i);
+        //a chaque click sur l'element
+        identifiant.addEventListener("click", function () {
+            //on ajoute une classe
+            identifiant.classList.add("fait");
+            tache.etat = 1;
+            //alert(i);
+            console.log(tache);
+        });
+        
+    });
+console.log(taches);
+//  var test = compteurTacheFait();
+//  console.log(test)
+}
 // ************************************
 // **********     CODE   **************
 // ************************************
 
 displayTasks(taches);
 
-document.getElementById('inserer').addEventListener('click',insertTask);
+document.getElementById('inserer').addEventListener('click', insertTask);
 
 
 //pour chaque tache
-taches.forEach((tache) => {
-    //i = l'id de l'element
-    var i=tache.id;
-    //pour l'element dont i est l'id
-    const identifiant = document.getElementById(i);
-    //a chaque click sur l'element
-    identifiant.addEventListener("click", function(){
-        //on ajoute une classe
-        identifiant.classList.add("fait");
-        //alert(i);
-    }
-    );
-});
-
+// var res = compteurTacheFait(tacheFait());
+// console.log(res);
